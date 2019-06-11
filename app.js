904f1de76909f10882d8e086582fce1daa902b9f
Sky Wickenden <skywickenden@users.noreply.github.com>
2019-06-11 17:35:01 +0100

    Install graphql

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const expressGraphQl = require("express-graphql");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("API served by express"));
+if (process.env.NODE_ENV !== "production") app.use(cors());
+
+app.use("/graphql", expressGraphQl({
+  schema: {},
+  graphiql: true
+}));
 
 module.exports = app;
